b3c858f67de0ca2325ba262a20a5fe364bf0dda7
sagor15 <sagorahammed1562@gmail.com>
2022-05-16 17:04:15 -0700

    doctor portal sever

---
+++ index.js
@@ -0,0 +1,156 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const res = require('express/lib/response');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kktrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function veryfyJwt(req, res, next) {
+    const authHeader = req?.headers?.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorize Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbiden access' })
+        }
+        req.decoded = decoded;
+        next()
+    });
+}
+
+
+
+
+
+
+async function run() {
+
+
+    try {
+        await client.connect();
+        const serviceCollection = client.db('doctors_portal').collection('services');
+        const bookingCollection = client.db('doctors_portal').collection('booking');
+        const usersCollection = client.db('doctors_portal').collection('users');
+
+
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        })
+
+        // post api create for booking
+
+
+        app.get('/booking', veryfyJwt, async (req, res) => {
+            const patient = req.query.patient;
+            const decodedEmail = req.decoded.email;
+            if (patient === decodedEmail) {
+                const query = { patient: patient }
+                const bookings = await bookingCollection.find(query).toArray();
+               return res.send(bookings);
+            }
+            else{
+                return res.status(403).send({message:'forbidden access'})
+            }
+
+        })
+
+
+
+
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exist = await bookingCollection.findOne(query);
+            if (exist) {
+                return res.send({ success: false, booking: exist });
+            }
+            const result = await bookingCollection.insertOne(booking);
+            res.send({ success: true, result });
+        })
+
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date ||
+                "May 14, 2022";
+            const services = await serviceCollection.find().toArray();
+
+
+            const query = { date: date }
+            const bookings = await bookingCollection.find(query).toArray();
+            services.forEach(service => {
+                const serviceBooking = bookings.filter(b => b.treatment === service.name);
+                const booked = serviceBooking.map(s => s.slot);
+                const available = service.slots.filter(s => !booked.includes(s));
+                service.slots = available;
+
+            })
+
+            res.send(services);
+        })
+
+
+
+
+
+
+    }
+    finally {
+
+    }
+
+
+
+
+
+
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('hello doctor uncle')
+})
+
+app.listen(port, () => {
+    console.log(`Doctors portal listening on port ${port}`)
+})
